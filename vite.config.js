2d11b41ab0a10b845e38c4417233b1a6ce587394
Ahmet Ayirga <ahmetayirga@Ahmets-MacBook-Pro.local>
2024-11-19 00:25:10 +0000

    Updated Vite and Replit configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { cspMiddleware } from './vite.middleware'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic'
-    })
-  ],
+  plugins: [react()],
   server: {
-    port: 4000,
-    strictPort: true,
-    hmr: true,
-    middlewares: [cspMiddleware()]
-  },
-  build: {
-    sourcemap: true,
+    host: '0.0.0.0',
+    port: 3000,
+    hmr: {
+      clientPort: 443
+    }
   }
 })
